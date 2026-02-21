57f198891817278de515e1d1f3709d52193688b3
EduardKhismatov <E.Khismatov@yandex.ru>
2026-02-21 16:19:53 +0600

    fix: getMyPortfolio

--- src/cache/cache.keys.ts
+++ src/cache/cache.keys.ts
@@ -1,3 +1,4 @@
 export const cacheRefreshToken = (token: string) => `refresh:${token}`;
 export const cacheRateLimit = (email: string) => `rate-limit:recovery:${email}`;
 export const cacheKey = (key: string) => `key:${key}`;
+export const cacheMyPortfolio = (id: string) => `my-portfolio:${id}`;
